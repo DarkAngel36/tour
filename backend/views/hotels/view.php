6f5f07c4cd6efd1798e161086008f3c1253219a1
a.druppov <adruppov@highwaymedia.ru>
2019-04-12 17:12:27 +0300

    switch to one domain

--- backend/views/hotels/view.php
+++ backend/views/hotels/view.php
@@ -7,7 +7,7 @@ use yii\widgets\DetailView;
 /* @var $model common\models\Hotels */
 
 $this->title = $model->name;
-$this->params['breadcrumbs'][] = ['label' => 'Hotels', 'url' => ['index']];
+$this->params['breadcrumbs'][] = ['label' => 'Отели', 'url' => ['index']];
 $this->params['breadcrumbs'][] = $this->title;
 \yii\web\YiiAsset::register($this);
 ?>
