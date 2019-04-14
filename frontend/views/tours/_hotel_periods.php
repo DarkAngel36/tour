3a8b4dcbe6fdea36686897edceb9de509548aa10
a.druppov <adruppov@highwaymedia.ru>
2019-04-14 10:00:30 +0300

    switch to one domain

---
+++ frontend/views/tours/_hotel_periods.php
@@ -0,0 +1,37 @@
+<?php
+
+?>
+
+<div class="select_tour_item clearfix">
+	<div class="about_tour_img2">
+		<img src="/images/park_sm1.png" alt=""/>
+	</div>
+	<div class="about_tour_text2">
+		<div class="mb13">
+			<h6 class="mb5">Анапа</h6>
+			<h6 class="md">База отдыха «Чайка»</h6>
+		</div>
+		<div class="about_tour_bottom">
+			<table class="about_tour_table version2">
+				<thead>
+				<tr>
+					<th>Период тура</th>
+					<th>Категория номера</th>
+					<th>Стоимость</th>
+					<th></th>
+				</tr>
+				</thead>
+				<tbody>
+				<tr>
+					<td data-title="Период тура">15.06.2018 — 23.06.2018</td>
+					<td data-title="Категория номера">Эконом</td>
+					<td data-title="Стоимость">от 20 000 руб.</td>
+					<td data-title="&nbsp;">
+						<a class="link-md md" href="javascript:;">Выбрать</a>
+					</td>
+				</tr>
+				</tbody>
+			</table>
+		</div>
+	</div>
+</div>
