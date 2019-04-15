bca6c31f7f8850a8642a5ddba57be52712926357
a.druppov <adruppov@highwaymedia.ru>
2019-04-15 14:54:34 +0300

    switch to one domain

--- frontend/views/tours/select2.php
+++ frontend/views/tours/select2.php
@@ -168,10 +168,10 @@
 				<div class="railway_container">
 					<div class="railway_box_wrap" data-toggle-check="off">
 						<div class="railway_box">
-							<?php for ($i = 1; $i <= 9; $i++): ?>
+							<?php for ($i = 0; $i <= 9; $i++): ?>
 								<?php
 								if (empty($places)) {
-									for ($i = 1; $i <= 54; $i++) {
+									for ($i = 0; $i <= 53; $i++) {
 										$places[$i] = 0;
 									}
 								}
@@ -180,31 +180,31 @@
 									<div class="railway_box_top mb40">
 										<!-- first_place/last_place меняется на reserved--!>
 										<div>
-											<div data-num="<?= $i ?>"
-											     class="railway_section_place <?= \common\models\Tours::getPlace($places[$i], 'first_place') ?> mb20">в
+											<div data-num="<?= $i * 6 ?>"
+											     class="railway_section_place <?= \common\models\Tours::getPlace($places[$i * 6], 'first_place') ?> mb20">в
 											</div>
-											<div data-num="<?= $i + 1 ?>"
-											     class="railway_section_place <?= \common\models\Tours::getPlace($places[$i], 'last_place') ?>">н
+											<div data-num="<?= $i * 6 + 1 ?>"
+											     class="railway_section_place <?= \common\models\Tours::getPlace($places[$i * 6 + 1], 'last_place') ?>">н
 											</div>
 										</div>
 										<div>
 											<img src="/images/icons/black_cloud.svg" alt="" />
 										</div>
 										<div>
-											<div data-num="<?= $i + 2 ?>"
-											     class="railway_section_place <?= \common\models\Tours::getPlace($places[$i + 2], 'first_place') ?> mb20">в
+											<div data-num="<?= $i * 6 + 2 ?>"
+											     class="railway_section_place <?= \common\models\Tours::getPlace($places[$i * 6 + 2], 'first_place') ?> mb20">в
 											</div>
-											<div data-num="<?= $i + 3 ?>"
-											     class="railway_section_place <?= \common\models\Tours::getPlace($places[$i], 'last_place') ?>">н
+											<div data-num="<?= $i * 6 + 3 ?>"
+											     class="railway_section_place <?= \common\models\Tours::getPlace($places[$i * 6 + 3], 'last_place') ?>">н
 											</div>
 										</div>
 									</div>
 									<div class="railway_box_bottom">
-										<div data-num="<?= $i + 4 ?>"
-										     class="railway_section_place <?= \common\models\Tours::getPlace($places[$i + 4], 'first_place') ?>">н
+										<div data-num="<?= $i * 6 + 4 ?>"
+										     class="railway_section_place <?= \common\models\Tours::getPlace($places[$i * 6 + 4], 'first_place') ?>">н
 										</div>
-										<div data-num="<?= $i + 5 ?>"
-										     class="railway_section_place <?= \common\models\Tours::getPlace($places[$i + 5], 'first_place') ?>">в
+										<div data-num="<?= $i * 6 + 5 ?>"
+										     class="railway_section_place <?= \common\models\Tours::getPlace($places[$i * 6 + 5], 'first_place') ?>">в
 										</div>
 									</div>
 								</div>
